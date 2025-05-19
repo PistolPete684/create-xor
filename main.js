8c1bb0086fece13289b1b884cb7f96f3ebddbc49
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-05-19 13:38:10 -0300

    adding js and sketch

---
+++ main.js
@@ -0,0 +1,7 @@
+function xor(a, b) {
+  // use the 'does not equal' comparison to return true when different and false when the same
+  // therefore; !== is equalivent to the xor operator 
+  return a !== b;
+}
+
+// const xor=(a, b)=>a!=b;
